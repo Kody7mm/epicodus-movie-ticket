ecdf227f52660b62c153551741bded44b6f96827
Josh Gustafson and Michelle Brecunier <silversevenfox@yahoo.com>
2016-01-26 10:31:07 -0800

    write spec for ticket constructor

--- spec/specs.js
+++ spec/specs.js
@@ -1,1 +1,9 @@
- 
+ describe('Ticket', function() {
+   it("create a ticket with the following properties", function() {
+     var newTicket = new Ticket('Superman', 'Morning', 'Adult');
+     expect(newTicket.movie).to.equal('Superman');
+     expect(newTicket.timeOfDay).to.equal('Morning');
+     expect(newTicket.age).to.equal('Adult');
+     expect(newTicket.price).to.equal();
+   });
+ });
