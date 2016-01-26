ebf5b83db3c2c2d65fa6ba170c4544c39b5afbc8
Josh Gustafson and Michelle Brecunier <silversevenfox@yahoo.com>
2016-01-26 10:37:53 -0800

    create movie constructor

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,9 @@ function Ticket(movie, timeOfDay, age) {
   this.timeOfDay = timeOfDay;
   this.age = age;
 }
+
+function Movie(title, rating, status) {
+  this.title = title;
+  this.rating = rating;
+  this.status = status;
+}
